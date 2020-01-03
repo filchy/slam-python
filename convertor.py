88bbd38f6b4e2e1c8ed498d714cf90aa3fe242c3
filchy <54070973+filchy@users.noreply.github.com>
2020-01-03 17:56:55 +0100

    Create convertor.py

---
+++ convertor.py
@@ -0,0 +1,14 @@
+from extractor import Extractor
+
+import cv2
+import numpy as np
+
+
+def cart2hom(array):
+	"""Convert array from Cartesian -> Homogenous (2 dimensions -> 3 dimensions)"""
+	if array.ndim == 1:
+		return np.array([0])
+
+	else:
+		array_3dim = np.asarray(np.vstack([array, np.ones(array.shape[1])]))
+		return array_3dim
