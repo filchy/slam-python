3f7a4828ae86b4ce7d5621d11a50426821ff6536
filchy <54070973+filchy@users.noreply.github.com>
2020-01-04 20:46:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-from slam import slam_run
+from slam import process
+from display import Display
+from pointmap import PointMap
 
 import cv2
 
 
+pmap = PointMap()
+display = Display()
+
 def main():
-	cap = cv2.VideoCapture("test_video2.mp4")
+    cap = cv2.VideoCapture("test_video3.mp4")
 
-	while cap.isOpened():
-		ret, frame = cap.read()
+    while cap.isOpened():
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (960, 540))
+        img, tripoints, kpts, matches = process(frame)
+        xyz = pmap.collect_points(tripoints)
 
-		slam_run(frame)
+        display.display_points2d(frame, kpts, matches)
+        display.display_points3d(xyz)
+        display.display_vid(frame)
 
-		if cv2.waitKey(1) == 27:
-			cv2.destroyAllWindows()
-			cap.release()
-			break
+        if cv2.waitKey(1) == 27:
+            cv2.destroyAllWindows()
+            cap.release()
+            break
 
 if __name__ == '__main__':
-	main()
+    main()
