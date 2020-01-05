8a08360e6f35e0f85c29f38f66a02ffddbee9e44
filchy <54070973+filchy@users.noreply.github.com>
2020-01-05 18:31:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,39 @@ from pointmap import PointMap
 
 import cv2
 
-
 pmap = PointMap()
 display = Display()
 
 def main():
-    cap = cv2.VideoCapture("test_video3.mp4")
+	cap = cv2.VideoCapture("test_video3.mp4")
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        frame = cv2.resize(frame, (960, 540))
-        img, tripoints, kpts, matches = process(frame)
-        xyz = pmap.collect_points(tripoints)
+	while cap.isOpened():
+		ret, frame = cap.read()
+		frame = cv2.resize(frame, (960, 540))
+		img, tripoints, kpts, matches = process(frame)
+		xyz = pmap.collect_points(tripoints)
 
-        display.display_points2d(frame, kpts, matches)
-        display.display_points3d(xyz)
-        display.display_vid(frame)
+		if ret:
+			if kpts is not None or matches is not None:
+				display.display_points2d(frame, kpts, matches)
+			else:
+				pass
 
-        if cv2.waitKey(1) == 27:
-            cv2.destroyAllWindows()
-            cap.release()
-            break
+			display.display_vid(frame)
+
+			if xyz is not None:
+				display.display_points3d(xyz)
+			else:
+				pass
+
+			if cv2.waitKey(1) == 27:
+				break
+
+		else:
+			break
+
+	cv2.destrouyAllWindows()
+	cap.release()
 
 if __name__ == '__main__':
-    main()
+	main()
